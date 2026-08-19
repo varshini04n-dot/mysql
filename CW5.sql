7397a14cd554518adfc6d3db68a1ef79e79fcd9a
Varshini <varshini04.n@gmail.com>
2026-08-19 12:51:16 +0530

    books

---
+++ CW5.sql
@@ -0,0 +1,7 @@
+INSERT INTO books VALUES(1, 'The Silent Patient', 'Alex Michaelides', 'Thriller', 399.00, 1200), (2, 'Atomic Habits', 'James Clear', 'Self-help', 499.00, 2000), (3, 'The Psychology of Money', 'Morgan Housel', 'Finance', 350.00, 1800);
+INSERT INTO bestsellers VALUES (4, 'Ikigai', 'Francesc Miralles', 'Philosophy', 300.00, 2500), (5, 'Think Like a Monk', 'Jay Shetty', 'Self-help', 450.00, 2200);
+SELECT title,author FROM books UNION SELECT title,author FROM bestsellers;
+SELECT * FROM `books` WHERE price>400;
+SELECT AVG(price) FROM books;
+SELECT COUNT(title)FROM books;
+SELECT title AS 'Book TITLE' ,author AS 'Written BY' FROM books;
